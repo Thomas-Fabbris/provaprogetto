8b62f4c2b63282fe2d44f706bbd7e693726a5790
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2024-10-18 23:17:59 +0200

    Organizzazione lavoro

--- provaprog/src/main/java/ingsw/provaprog/App.java
+++ provaprog/src/main/java/ingsw/provaprog/App.java
@@ -1,10 +1,7 @@
 package ingsw.provaprog;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //TODO: Implementare somma, differenza e divisione tra due numeri
     }
 }
